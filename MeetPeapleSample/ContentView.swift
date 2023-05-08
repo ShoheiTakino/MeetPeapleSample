0a60c0a1a65e635d7f522d3ba72557f56c1b0f61
ShoheiTakino <shohei0522penguin@gmail.com>
2023-05-08 20:46:51 +0900

    Initial Commit

---
+++ MeetPeapleSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MeetPeapleSample
+//
+//  Created by 滝野翔平 on 2023/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetPeapleSample/MeetPeapleSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetPeapleSampleApp.swift
+//  MeetPeapleSample
+//
+//  Created by 滝野翔平 on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct MeetPeapleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
