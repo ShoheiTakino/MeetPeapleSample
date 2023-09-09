6760d9ad1f7dbd10eaa384f2c1a9bbba566ae53e
ShoheiTakino <shohei0522penguin@gmail.com>
2023-09-09 20:20:34 +0900

    ✨スクリーンショットの監視

--- MeetPeapleSample/MeetPeapleSampleApp.swift
+++ MeetPeapleSample/MeetPeapleSampleApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct MeetPeapleSampleApp: App {
+    @State private var isScreenshotTaken = false
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
+                    // スクリーンショットが撮られたときに実行される処理
+                    print(#function, "aaa")
+                    isScreenshotTaken = true
+                }
+                .alert(isPresented: $isScreenshotTaken) {
+                    Alert(title: Text("⚠️警告"), message: Text("このアプリでは、スクリーンショットを撮ることは禁止されています。\n何度もスクリーンショットを撮ると大会の対象になります。\n皆様が安心してご利用していただけるようにご理解のほどよろしくお願いします。"), dismissButton: .default(Text("おけ"), action: {
+                        isScreenshotTaken = false
+                    }))
+                }
         }
     }
 }
