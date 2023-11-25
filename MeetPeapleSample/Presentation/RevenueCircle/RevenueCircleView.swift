bcbf7bf87d47e8088561e46173e6be41373c3e55
ShoheiTakino <shohei0522penguin@gmail.com>
2023-11-25 10:48:37 +0900

    ✨円グラフのファイル追加

---
+++ MeetPeapleSample/Presentation/RevenueCircle/RevenueCircleView.swift
@@ -0,0 +1,20 @@
+//
+//  RevenueCircleView.swift
+//  MeetPeapleSample
+//
+//  Created by 滝野翔平 on 2023/11/25.
+//
+
+import SwiftUI
+
+struct RevenueCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RevenueCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RevenueCircleView()
+    }
+}
