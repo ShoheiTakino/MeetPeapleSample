e34f1b5a798a5020db6747840606577da4d218a5
ShoheiTakino <shohei0522penguin@gmail.com>
2023-12-03 19:27:29 +0900

    ✨インプットバー

--- MeetPeapleSample/Presentation/DirectMessage/View/DirectMessageView.swift
+++ MeetPeapleSample/Presentation/DirectMessage/View/DirectMessageView.swift
@@ -9,9 +9,50 @@ import SwiftUI
 
 struct DirectMessageView: View {
     var body: some View {
-        Text("DirectMessageScreen")
+        VStack {
+            // チャットメッセージの表示領域（ダミー）
+            Text("Chat Messages Here")
+            Spacer()
+            // インプットバー
+            ChatInputView()
+        }
+        .padding()
     }
 }
+
+struct ChatInputView: View {
+    @State private var messageText: String = ""
+
+    var body: some View {
+        HStack {
+            VStack {
+                Spacer()
+                TextEditor(text: $messageText)
+                    .frame(height: 100)
+                    .padding(10)
+                    .background(.gray)
+                    .cornerRadius(15)
+            }
+            VStack {
+                Spacer()
+                Button(action: {
+                    // メッセージ送信の処理をここに追加
+                    print("メッセージ送信: \(messageText)")
+                    // 送信後にテキストをクリア
+                    messageText = ""
+                }) {
+                    Text("送信")
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .background(Color.blue)
+                        .cornerRadius(15)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
 
 struct DirectMessage_Previews: PreviewProvider {
     static var previews: some View {
